fb329c8dab76b30f904a14444501ea6e60900aef
ayol377 <ayaalali@protonmail.com>
2025-11-11 14:42:29 +0500

    debug hash

--- external-import/sentinelone-incidents/src/external_import_connector/converter_to_stix.py
+++ external-import/sentinelone-incidents/src/external_import_connector/converter_to_stix.py
@@ -294,24 +294,20 @@ class ConverterToStix:
         sha1 = threat_info.get("sha1", "")
         sha256 = threat_info.get("sha256", "")
         md5 = threat_info.get("md5", "")
+        self.helper.connector_logger.info("SHA1: " + sha1)
+        self.helper.connector_logger.info("SHA256: " + sha256)
+        self.helper.connector_logger.info("MD5: " + md5)
         hashes = {k: v for k, v in [("SHA-256", sha256), ("SHA-1", sha1), ("MD5", md5)] if v}
         observables = []
 
-        if threat_info.get("isFileless", "") == "False":
-            if hashes:
-                observable = stix2.File(
+        if hashes:
+            self.helper.connector_logger.info("Creating Stix Observable with hashes: " + str(hashes))
+            observable = stix2.File(
                     type="file",
                     name=threat_info.get("threatName", ""),
                     hashes=hashes,
                     object_marking_refs=[stix2.TLP_RED],
                 )
-            else:
-                observable = stix2.File(
-                    type="file",
-                    name=threat_info.get("threatName", ""),
-                    object_marking_refs=[stix2.TLP_RED],
-                )
-                
             observables.append(
                 self.create_relationship(cti_incident_id, observable["id"], "related-to")
             )
